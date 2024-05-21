91ea5e5f0b9987bd26b3cc7b95187638ecc6c4d6
Dinoh04 <dino.rainimarolahy@gmail.com>
2024-05-21 13:44:06 +0300

    Feat: create application express

---
+++ index.ts
@@ -0,0 +1,12 @@
+
+import express, {Request, Response} from 'express';
+const app = express();
+const PORT = 5000;
+
+app.get('/',(req : Request, res: Response)=>{
+    res.send('Hello you');
+})
+
+app.listen(PORT,()=>{
+    console.log(`The serveur is running on port: http://localhost:${PORT}`);
+})
