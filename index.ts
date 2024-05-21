8e790df9c21c9d2b3e60ece05e787941da630bf7
Dinoh04 <dino.rainimarolahy@gmail.com>
2024-05-21 16:39:01 +0300

    Feat: add bcrypt and create registration for admin

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
 import express, {Request, Response} from 'express';
+import cors from 'cors';
+import { createAdmin } from './Controller/admin';
+import { getAllAdmin } from './Controller/admin';
 const app = express();
 const PORT = 5000;
 
-app.get('/',(req : Request, res: Response)=>{
-    res.send('Hello you');
-})
+app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+  );
+
+
+app.post('/admin', createAdmin);
+app.get('/admin',getAllAdmin);
+
 
 app.listen(PORT,()=>{
     console.log(`The serveur is running on port: http://localhost:${PORT}`);
