71abe6b9bb19877b051a64a5313d5a789beb4ce9
Dinoh04 <dino.rainimarolahy@gmail.com>
2024-05-21 14:56:29 +0300

    Feat: make relation with two table in prisma schema

--- Controller/admin.ts
+++ Controller/admin.ts
@@ -1,1 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+import express, { Request, Response } from "express";
+const prisma = new PrismaClient();
+const amdin = express();
+
+amdin.use(express.json());
+
+export const createAdmin = async (req: Request, res: Response) =>{
+    const {} = req.body;
+}
