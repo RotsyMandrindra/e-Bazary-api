9ab77af4c469dfe01c3185e9f8d2daa2b1ea7e85
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2024-05-21 11:08:45 +0300

    chore: reorganized the project

--- migration/admin.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS user_admin(
-    user_admin_id serial primary key,
-    email varchar(200) not null, 
-    name varchar(200) not null, 
-    password varchar(200) not null
-);
--- migration/images.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS images(
-    images_id serial primary key,
-    product_id int references car(product_id),
-    url varchar(50) not null
-);
