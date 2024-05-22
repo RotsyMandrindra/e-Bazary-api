e94c876fc249b36f1f585cf1ecc2bc9dfdaae027
Dinoh04 <dino.rainimarolahy@gmail.com>
2024-05-22 21:27:05 +0300

    Feat: handle error when admin create with the same email

--- Controller/admin.ts
+++ Controller/admin.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
 import bcrypt from 'bcrypt';
+import { error } from "console";
 
 const prisma = new PrismaClient(
     { log : ["query"]}
@@ -17,7 +18,15 @@ export const createAdmin = async (req: Request, res: Response) =>{
      return res.status(400).json({ error: "Admin must contains email, name, password"})
     }
     
-    
+   const findAmin = await prisma.admin.findUnique({
+    where: {
+        email : email
+    }
+   })
+
+    if(findAmin){
+        return res.status(500).json({ error: "Email already taken, use an another email"});
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user_admin = await prisma.admin.create({
             data: {
@@ -27,13 +36,13 @@ export const createAdmin = async (req: Request, res: Response) =>{
             },
         });
         res.json({ user_admin });
-        console.log(user_admin);
+       
 }
 
 
 export const getAllAdmin = async (req: Request, res: Response) =>{
     const admin = await prisma.admin.findMany({})
-       return  res.json({ admin})  
+       return  res.json({ admin })  
 }
 
 
